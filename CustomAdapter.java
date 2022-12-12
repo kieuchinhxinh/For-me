a04fcb6a1c48d7e9d8d5b43c1d7c5a830af45a3e
kieuchinhxinh <80748999+kieuchinhxinh@users.noreply.github.com>
2022-12-12 14:16:36 +0700

    Update CustomAdapter.java

--- CustomAdapter.java
+++ CustomAdapter.java
@@ -1,4 +1,4 @@
-package com.example.lastedition;
+package com.example.lastdemo;
 
 import android.app.Activity;
 import android.content.Context;
@@ -17,7 +17,7 @@ import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.lastedition.UpdateActivity;
+import com.example.lastdemo.UpdateActivity;
 
 import java.util.ArrayList;
 
@@ -25,14 +25,17 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     private Context context;
     private Activity activity;
-    private ArrayList trip_id, trip_title, trip_description, trip_participant;
+    private ArrayList trip_id, trip_title,trip_transport, trip_date, trip_returndate, trip_description, trip_participant;
 
-    CustomAdapter(Activity activity, Context context, ArrayList trip_id, ArrayList trip_title, ArrayList trip_description,
+    CustomAdapter(Activity activity, Context context, ArrayList trip_id, ArrayList trip_title,ArrayList trip_transport ,ArrayList trip_date, ArrayList trip_returndate, ArrayList trip_description,
                   ArrayList trip_participant){
         this.activity = activity;
         this.context = context;
         this.trip_id = trip_id;
         this.trip_title = trip_title;
+        this.trip_transport = trip_transport;
+        this.trip_date = trip_date;
+        this.trip_returndate = trip_returndate;
         this.trip_description = trip_description;
         this.trip_participant = trip_participant;
     }
@@ -50,6 +53,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
         holder.trip_id_txt.setText(String.valueOf(trip_id.get(position)));
         holder.trip_title_txt.setText(String.valueOf(trip_title.get(position)));
+        holder.trip_transport_txt.setText(String.valueOf(trip_transport.get(position)));
+        holder.trip_date_txt.setText(String.valueOf(trip_date.get(position)));
+        holder.trip_returndate_txt.setText(String.valueOf(trip_returndate.get(position)));
+
         holder.trip_description_txt.setText(String.valueOf(trip_description.get(position)));
         holder.trip_participant_txt.setText(String.valueOf(trip_participant.get(position)));
         //Recyclerview onClickListener
@@ -59,6 +66,12 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
                 Intent intent = new Intent(context, UpdateActivity.class);
                 intent.putExtra("id", String.valueOf(trip_id.get(position)));
                 intent.putExtra("title", String.valueOf(trip_title.get(position)));
+                intent.putExtra("transport", String.valueOf(trip_transport.get(position)));
+
+                intent.putExtra("date", String.valueOf(trip_date.get(position)));
+                intent.putExtra("returndate", String.valueOf(trip_returndate.get(position)));
+
+
                 intent.putExtra("description", String.valueOf(trip_description.get(position)));
                 intent.putExtra("participant", String.valueOf(trip_participant.get(position)));
                 activity.startActivityForResult(intent, 1);
@@ -75,13 +88,16 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView trip_id_txt, trip_title_txt, trip_description_txt,  trip_participant_txt;
+        TextView trip_id_txt, trip_title_txt, trip_transport_txt, trip_date_txt, trip_returndate_txt,  trip_description_txt,  trip_participant_txt;
         LinearLayout mainLayout;
 
         MyViewHolder(@NonNull View itemView) {
             super(itemView);
             trip_id_txt = itemView.findViewById(R.id.trip_id_txt);
             trip_title_txt = itemView.findViewById(R.id.trip_title_txt);
+            trip_transport_txt = itemView.findViewById(R.id.trip_transport_txt);
+            trip_date_txt = itemView.findViewById(R.id.trip_date_txt);
+            trip_returndate_txt = itemView.findViewById(R.id.trip_returndate_txt);
             trip_description_txt = itemView.findViewById(R.id.trip_description_txt);
             trip_participant_txt = itemView.findViewById(R.id.trip_participant_txt);
             mainLayout = itemView.findViewById(R.id.mainLayout);
